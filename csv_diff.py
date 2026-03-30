ff3fa1724e79bb65df60ecb5b01e6018eda78d96
Mike Lynch <m.lynch@sydney.edu.au>
2026-03-30 14:12:02 +1100

    Better printing of column-level diffs

--- csv_diff.py
+++ csv_diff.py
@@ -3,15 +3,32 @@ from argparse import ArgumentParser
 from pathlib import Path
 
 
+def abbrev(v):
+    if v is not None:
+        return v[:20]
+    else:
+        return v
+
+
 def csv_diff(a, b):
     dfa = pl.read_csv(a)
     dfb = pl.read_csv(b)
+    cols = dfa.columns
     mismatches = (
         dfa.filter(~dfa.hash_rows().is_in(dfb.hash_rows().implode()))
            .join(dfb, on="ID", how="left", suffix="_dfb")
        )
-    return mismatches
-
+    for row in mismatches.to_dicts():
+        print("-- " + row["ID"])
+        for col in cols:
+            if f"{col}_dfb" in row:
+                va = row[col]
+                vb = row[f"{col}_dfb"]
+                if va != vb:
+                    ava = abbrev(va)
+                    avb = abbrev(vb)
+                    print(f"   A {col}: '{ava}'")
+                    print(f"   B {col}: '{avb}'")
 
 
 
@@ -28,10 +45,7 @@ def main():
         help="CSV file B",
     )
     args = ap.parse_args()
-    with pl.Config() as cfg:
-        cfg.set_tbl_cols(-1)
-        mismatches = csv_diff(args.a, args.b)
-        print(mismatches)
+    csv_diff(args.a, args.b)
 
 
 if __name__ == "__main__":
